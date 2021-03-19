1d8e0c4ecaf6f19713291075e0d505fe0f8156f7
AerR <57688724+AeronRoemer@users.noreply.github.com>
2021-03-19 15:54:49 -0600

    Passing Wave 2 tests

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -1,3 +1,6 @@
+from statistics import mean # new requirement 
+from collections import Counter # built in to python
+
 def create_movie(movie_title, genre, rating):
     if movie_title and genre and rating:
         return {
@@ -21,3 +24,21 @@ def watch_movie(user_data, movie):
             user_data['watched'].append(item)
             user_data['watchlist'].remove(item)
     return user_data
+
+def get_watched_avg_rating(user_data):
+    if user_data['watched']:
+        # using mean() from statistics since numpy mean() takes Array, not List
+        return mean(movie['rating'] for movie in user_data['watched']) # list comprehension replaces for loop
+    else:
+        return 0
+
+def get_most_watched_genre(user_data):
+    if not user_data['watched']:
+        return None
+    else:
+        genre_list = [item['genre'] for item in user_data['watched']]
+        # returns each genre in a list
+        genre_count = Counter(genre_list).most_common() 
+        # creates an unordered collection of data with count information from python's built in Counter from collections module
+        return genre_count[0][0]
+        
