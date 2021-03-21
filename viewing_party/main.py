1799fcb48a30cf0db41b7c9a7b5c222c7aa0be1b
Summer <summmick@gmail.com>
2021-03-20 19:30:07 -1000

    Created watch_movie function to sastify last 3 test in test_wave_01

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -17,3 +17,10 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
 
     return user_data
+
+def watch_movie(user_data, title):
+    for x in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][x]["title"] == title:
+            user_data["watched"].append(user_data["watchlist"][x])
+            user_data["watchlist"].pop(x)
+    return user_data
