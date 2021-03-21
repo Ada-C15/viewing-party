e6523b9704d559e276cdaaf5bca5844ce0b348ee
Tammy <iptmhatuw@gmail.com>
2021-03-20 20:12:03 -0700

    wave 02 done

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -34,10 +34,43 @@ def watch_movie(user_data, title):
                     user_data["watchlist"].remove(movie)
                     user_data["watched"].append(movie)
                     return user_data
+                # else:
+                #     return user_data
             # return use_data
         # return use_data
-
     return user_data
+
+def get_watched_avg_rating(user_data):
+    total_rating = 0
+    for v in user_data.values():
+        if v == []:
+            return 0.0
+        else:
+            for movie in v:
+                total_rating += movie["rating"]
+            avg = total_rating / len(v)
+            return avg
+
+def get_most_watched_genre(user_data):
+    genre_map = {}
+    for v in user_data.values():
+        for movie in v:
+            x = movie["genre"]
+            if x not in genre_map:
+                genre_map[x] = 1
+            else:
+                genre_map[x] += 1
+    
+    current_max = 0
+    for k, v in genre_map.items():
+        if v > current_max:
+            current_max = v
+    
+    for k, v in genre_map.items():
+        if v == current_max:
+            return k
+
+
 
 
 
