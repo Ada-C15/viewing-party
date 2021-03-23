85dc5a041a7f07d3c0290a81b87822e28b6169fc
Tammy <iptmhatuw@gmail.com>
2021-03-23 01:12:18 -0700

    wave 04 done

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -70,6 +70,81 @@ def get_most_watched_genre(user_data):
         if v == current_max:
             return k
 
+def get_unique_watched(user_data):
+
+    mine_watched = {}
+    for movie in user_data["watched"]:
+        mine_watched[movie["title"]] = True
+    
+    friends_watched = {}
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched[movie["title"]] = True
+
+    diff = []
+    for title in mine_watched.keys():
+        if title not in friends_watched:
+            diff.append({"title": title})
+    return (diff)
+
+    # movie_map = {}
+    # for k,v in user_data.items():
+    #     if k == "watched":
+    #         for movie in v:
+    #             x = movie["title"]
+    #             if x not in movie_map:
+    #                 movie_map[x] = 0  
+    #     if k == "friends":
+    #         for friend in v:
+    #             for v in friend.values():
+    #                 for title in v:
+    #                     y = title["title"]
+    #                     if y in movie_map:
+    #                         movie_map[y] += 1
+
+    # # Return a list of dictionaries, that represents a list of movies
+    # holder = []
+    # for k,v in movie_map.items():
+    #     if v == 0:
+    #         holder.append(k)
+
+    # result = [{"title": i} for i in holder]
+
+    # return result
+
+def get_friends_unique_watched(user_data):
+
+    mine_watched = {}
+    for movie in user_data["watched"]:
+        mine_watched[movie["title"]] = True
+    
+    friends_watched = {}
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched[movie["title"]] = True
+
+    diff = []
+    for title in friends_watched.keys():
+        if title not in mine_watched:
+            diff.append({"title": title})
+    return (diff)     
+
+def get_available_recs(user_data):
+
+    friends_watched = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched.append(movie)
+    
+    result = []
+    for movie in friends_watched:
+        if movie["host"] in user_data["subscriptions"]:
+            result.append(movie)
+    # [{'title': 'Title A', 'host': 'Service A'}, {'title': 'Title A', 'host': 'Service A'}, {'title': 'Title B', 'host': 'Service B'}]
+    rec_list = [i for n, i in enumerate(result) if i not in result[n + 1:]]
+    return rec_list
+
+
 
 
 
