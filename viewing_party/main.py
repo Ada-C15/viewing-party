24381cd010ffc66f14d3ae3a0b2fe0167170da36
Jen Lim <jennifer.lim07@gmail.com>
2021-03-25 09:29:00 -0700

    refactor code for wave 02 through 05

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -9,13 +9,16 @@ def create_movie(movie_title, genre, rating):
         movie_dict["rating"] = rating
         return movie_dict
 
+
 def add_to_watched(user_data, movie):
     user_data["watched"] = [movie]
     return user_data
 
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"] = [movie]
     return user_data
+
 
 def watch_movie(user_data, title):
     for movie in user_data["watchlist"]:
@@ -27,25 +30,24 @@ def watch_movie(user_data, title):
 
 # test_wave_02
 def get_watched_avg_rating(user_data):
-    average_rating = 0.0
+    total_rating = 0.0
     if len(user_data["watched"]) == 0:
-        return average_rating
+        return total_rating
 
-    for movies in user_data.values():
-        total_rating = 0.0
-        for data in movies:
-            total_rating += data["rating"]
-        average_rating = total_rating/len(movies)
-    return average_rating
+    for movies in user_data["watched"]:
+        total_rating += movies["rating"]
+
+    return total_rating/len(user_data["watched"])
+
 
 def get_most_watched_genre(user_data):
     genre_dict = {}
-    for movie in user_data.values():
-        for data in movie:
-            if data["genre"] not in genre_dict:
-                genre_dict[data["genre"]] = 1
-            else:
-                genre_dict[data["genre"]] += 1
+    for movie in user_data["watched"]:
+        if movie["genre"] not in genre_dict:
+            genre_dict[movie["genre"]] = 1
+        else:
+            genre_dict[movie["genre"]] += 1
+            
     for key, value in genre_dict.items():
         if value == max(genre_dict.values()):
             return key
@@ -53,51 +55,40 @@ def get_most_watched_genre(user_data):
 
 # test_wave_03
 def get_unique_watched(user_data):
-    not_watched_list = []
-    track_list = []
+    movies_list = [movie for movie in user_data["watched"]]
 
-    for movie in user_data["watched"]:
-        track_list.append(movie["title"])
+    for friend in user_data["friends"]:
+        for movies_watched in friend["watched"]:
+            if movies_watched in movies_list:
+                movies_list.pop(friend["watched"].index(movies_watched))
 
-    for i in range(len(user_data["friends"])):
-        for j in range(len(user_data["friends"][i]["watched"])):
-            if user_data["friends"][i]["watched"][j]["title"] in track_list:
-                track_list.pop(user_data["friends"][i]["watched"].index(user_data["friends"][i]["watched"][j]))
+    return movies_list
 
-    for movie in track_list:
-        not_watched_list.append({"title" : movie})
-
-    return not_watched_list
 
 def get_friends_unique_watched(user_data):
-    not_watched_list = []
-    track_list = []
+    movies_list = []
 
-    for i in range(len(user_data["friends"])):
-        for j in range(len(user_data["friends"][i]["watched"])):
-            if user_data["friends"][i]["watched"][j]["title"] not in track_list:
-                track_list.append(user_data["friends"][i]["watched"][j]["title"])
+    for friend in user_data["friends"]:
+        for movies_watched in friend["watched"]:
+            if movies_watched not in movies_list:
+                movies_list.append(movies_watched)
+    
+    for movies_watched in user_data["watched"]:
+        if movies_watched in movies_list:
+            movies_list.pop(movies_list.index(movies_watched))
 
-    for i in range(len(user_data["watched"])):
-        title_name = user_data["watched"][i]["title"]
-        if title_name in track_list:
-            track_list.pop(track_list.index(title_name))
-
-    for movie in track_list:
-        not_watched_list.append({"title" : movie})
-
-    return not_watched_list
+    return movies_list
 
 
 # test_wave_04
 def get_available_recs(user_data):
     recommended_movies = []
 
-    for movies in user_data["friends"]:
-        for i in range(len(movies["watched"])):
-            if movies["watched"][i]["host"] in user_data["subscriptions"]:
-                if movies["watched"][i] not in recommended_movies:
-                    recommended_movies.append(movies["watched"][i])
+    for friend in user_data["friends"]:
+        for movies_watched in friend["watched"]:
+            if movies_watched["host"] in user_data["subscriptions"] and \
+                    movies_watched not in recommended_movies:
+                recommended_movies.append(movies_watched)
 
     return recommended_movies
 
@@ -117,24 +108,26 @@ def get_new_rec_by_genre(user_data):
     except ValueError:
         return recommended_movies
 
-    for data in user_data["friends"]:
-        for i in range(len(data["watched"])):
-            if data["watched"][i]["genre"] == fav_genre and data["watched"][i]["title"] not in movies_list:
-                recommended_movies.append(data["watched"][i])
+    for friend in user_data["friends"]:
+        for watched_movie in friend["watched"]:
+            if watched_movie["genre"] == fav_genre and \
+                    watched_movie["title"] not in movies_list:
+                recommended_movies.append(watched_movie)
 
     return recommended_movies
+
 
 def get_rec_from_favorites(user_data):
     recommended_movies = []
 
-    not_watched_listed = []
     friends_list = []
-    for data in user_data["friends"]:
-        for i in range(len(data["watched"])):
-            friends_list.append(data["watched"][i])
+    for friend in user_data["friends"]:
+        for watched_movie in friend["watched"]:
+            friends_list.append(watched_movie)
 
-    for i in range(len(user_data["watched"])):
-        if user_data["watched"][i] not in friends_list and user_data["watched"][i] in user_data["favorites"]:
-            recommended_movies.append(user_data["watched"][i])
+    for watched_movie in user_data["watched"]:
+        if watched_movie not in friends_list and \
+                watched_movie in user_data["favorites"]:
+            recommended_movies.append(watched_movie)
 
     return recommended_movies
