e58296bbe3ea96a649fbab7bee16b5844f8e25c1
Summer <summmick@gmail.com>
2021-03-24 18:36:29 -1000

    Created Functions to satisfy test_wave_02

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 def create_movie(movie_title, genre, rating):
     if movie_title is None or genre is None or rating is None:
         return None 
@@ -19,8 +21,37 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 def watch_movie(user_data, title):
-    for x in range(len(user_data["watchlist"])):
-        if user_data["watchlist"][x]["title"] == title:
-            user_data["watched"].append(user_data["watchlist"][x])
-            user_data["watchlist"].pop(x)
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+
     return user_data
+
+def get_watched_avg_rating(user_data):
+    if not user_data["watched"]:
+        return 0
+
+    ratings = []
+    for movie in user_data["watched"]:
+        ratings.append(movie["rating"])
+
+    return mean(ratings)
+
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
+
+    genres = {}
+    for movie in user_data["watched"]:
+        for k, v in movie.items():
+            if k == "genre":
+                if v in genres:
+                    genres[v] += 1
+                else:
+                    genres[v] = 1 
+
+
+    most_watched = max(genres)
+    return most_watched
+
