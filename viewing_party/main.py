699044f204d66b4af542ee006738ecc2941659ce
Summer <summmick@gmail.com>
2021-03-26 13:22:23 -1000

    Created Functions to satisfy test_wave_03

--- viewing_party/main.py
+++ viewing_party/main.py
@@ -59,5 +59,34 @@ def get_most_watched_genre(user_data):
             count = v
             most_watched = k 
 
-    return most_watched 
-        
+    return most_watched
+
+def get_unique_watched(user_data):
+    watched_source = user_data["watched"] 
+    friend_1 = user_data["friends"][0]["watched"]
+    friend_2 = user_data["friends"][1]["watched"]
+    
+    unique_movies = []
+    for movie in watched_source:
+        if movie not in friend_1 and movie not in friend_2:
+            unique_movies.append({"title": movie["title"]})
+            
+    return unique_movies
+
+
+def get_friends_unique_watched(user_data):
+    watched_source = user_data["watched"] 
+    friend_1 = user_data["friends"][0]["watched"]
+    friend_2 = user_data["friends"][1]["watched"]
+
+    friends_unique_movies = []
+    for movie in friend_1:
+        if movie not in watched_source:
+            friends_unique_movies.append({"title": movie["title"]})
+    
+    for movie in friend_2:
+        if movie not in watched_source and movie not in friends_unique_movies:
+            friends_unique_movies.append({"title": movie["title"]})
+
+    return friends_unique_movies
+
